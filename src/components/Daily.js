4dd9bf69f32f070ed8813ebe0c4a11820b1c27c5
Unknown <Zachary.Lynch1337@gmail.com>
2018-10-22 21:09:09 -0700

    Added days of the week to the 7 day forcast

--- src/components/Daily.js
+++ src/components/Daily.js
@@ -3,10 +3,14 @@ import '../CSS/Daily.css';
 
 
 const daily = (props) => {
-    console.log(props);
+    let timeStamp = props.day.time;
+    let a = new Date(timeStamp * 1000);
+    const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat'];
+
     return (
         <div className="daily-container">
         <div className="seven-day">
+            <h1>{days[a.getDay()]}</h1>
             <h1>{Math.floor(props.day.apparentTemperatureHigh)}</h1>
             <div className={props.day.icon}></div>
             <h1>{Math.floor(props.day.apparentTemperatureLow)}</h1>
